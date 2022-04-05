380eefbc6113b26adbb9c6cd7069c5370cf0cc73
Sahil Satralkar <sahil.satralkar@gmail.com>
2022-04-05 23:20:12 +0530

    Initial Commit

---
+++ Singletons_Playground.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+class Singleton {
+    static let shared = Singleton()
+    
+    private init() {
+        
+    }
+    private var number = 1
+    func printSomething() {
+        number += 1
+        print("This is printSomething function = \(number)")
+    }
+}
+
+
+let testObj = Singleton.shared
+//testObj.printSomething()
+
+
+class SingletionSub : Singleton {
+    func printSingletonSub() {
+        print("This is printSingletonSub")
+    }
+    override func printSomething() {
+        print("printSomething from SingletonSub class")
+    }
+}
+
+let testObjSingSubclass = SingletionSub()
+
+testObjSingSubclass.printSingletonSub()
+testObjSingSubclass.printSomething()
+
+let test = Singleton()
+test.printSomething()
+test.printSomething()
+testObj.printSomething()
