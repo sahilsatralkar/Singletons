d778f40ac56743c7aa82b0b436ff56696ae9677d
Sahil Satralkar <sahil.satralkar@gmail.com>
2022-04-11 19:52:22 +0530

    commit

--- Singletons_Playground.playground/Contents.swift
+++ Singletons_Playground.playground/Contents.swift
@@ -1,38 +1,59 @@
 import UIKit
 
 class Singleton {
+    
     static let shared = Singleton()
     
+    var parentCounter = 0
+    
+    func incrementParentCount() {
+        parentCounter += 1
+        print("Value of parentCounter is \(parentCounter)")
+    }
+}
+
+let singletonObject1 = Singleton.shared
+singletonObject1.incrementParentCount()
+singletonObject1.incrementParentCount()
+
+let singletonObject2 = Singleton.shared
+singletonObject2.incrementParentCount()
+singletonObject2.incrementParentCount()
+
+class SingletionSubClass : Singleton {
+    
+    var childCounter = 0
+    
+    func incrementChildCounter() {
+        childCounter += 1
+        print("Value of child Counter is \(childCounter)")
+    }
+    
+    override func incrementParentCount() {
+        self.parentCounter += 1
+        print("Value of parentCounter is \(parentCounter)")
+    }
+}
+
+let childSingletonObj1 = SingletionSubClass()
+childSingletonObj1.incrementChildCounter()
+childSingletonObj1.incrementParentCount()
+childSingletonObj1.incrementParentCount()
+
+
+class SingletonNoSubclassing {
+    
+    static let shared = SingletonNoSubclassing()
+    
+    var parentCounter : Int
+    
     private init() {
-        
+        self.parentCounter = 0
     }
-    private var number = 1
-    func printSomething() {
-        number += 1
-        print("This is printSomething = \(number)")
+    
+    func incrementParentCount() {
+        parentCounter += 1
+        print("Value of parentCounter is \(parentCounter)")
     }
+
 }
-
-
-let testObj = Singleton.shared
-//testObj.printSomething()
-
-
-class SingletionSub : Singleton {
-    func printSingletonSub() {
-        print("This is printSingletonSub")
-    }
-    override func printSomething() {
-        print("printSomething from SingletonSub class")
-    }
-}
-
-let testObjSingSubclass = SingletionSub()
-
-testObjSingSubclass.printSingletonSub()
-testObjSingSubclass.printSomething()
-
-let test = Singleton()
-test.printSomething()
-test.printSomething()
-testObj.printSomething()
